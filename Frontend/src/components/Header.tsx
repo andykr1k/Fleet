af5b3cf6668ad480e6061852420f646626ef7c7c
andykr1k <69021102+andykr1k@users.noreply.github.com>
2025-02-12 12:44:49 -0800

    Fixed header

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -20,7 +20,7 @@ export default function Header({ title }: HeaderProps) {
   };
 
   return (
-    <div className="relative z-[10] max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 flex justify-between items-center">
+    <div className="relative z-[10] max-w-7xl mx-auto pt-8 flex justify-between items-center">
       <div className="flex items-center space-x-2">
         <Link to={"/"} className="w-6 h-6 bg-gradient-to-br from-blue-500 to-purple-600 rounded-full" />
         <span className="text-xl font-semibold bg-gradient-to-r from-blue-400 to-purple-400 bg-clip-text text-transparent">
